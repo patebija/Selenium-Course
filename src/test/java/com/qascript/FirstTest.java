4ed877e5bb3dd25cd13491cacc5b19636332b4e0
Bijan Patel <bijan@binary.com>
2020-08-20 00:20:16 +0530

    Initial commit

---
+++ src/test/java/com/qascript/FirstTest.java
@@ -0,0 +1,24 @@
+package com.qascript;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirstTest {
+
+
+    @Test
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+    }
+
+
+}
+
+
