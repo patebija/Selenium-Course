edd8960ff4dccc82fc59474ab15645a80165540d
bijan <bijan@qascript.com>
2021-06-17 22:32:18 +0530

    Update Pom

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -15,6 +15,7 @@ public class FirstTest {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
+        options.addArguments("disable-gpu");
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
