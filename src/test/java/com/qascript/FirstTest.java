f1a151b6d1e61b3c50d35189c7d3608d6e9fff06
bijan <bijan@qascript.com>
2021-06-19 01:06:36 +0530

    Update driver path

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -12,11 +12,10 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--disable-gpu","--remote-debugging-port=9222",
-                "--ignore-certificate-errors","--disable-extensions","--no-sandbox",
-                "--disable-dev-shm-usage");
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
